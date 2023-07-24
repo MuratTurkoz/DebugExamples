0bb2d72ab01e6aea89671986085b136e183d08ef
MuratTurkoz <44347576+MuratTurkoz@users.noreply.github.com>
2023-07-25 00:29:14 +0300

    Added Debug methods

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,8 +1,11 @@
 using JetBrains.Annotations;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
 
 public class Player : MonoBehaviour
 {
@@ -14,44 +17,75 @@ public class Player : MonoBehaviour
         name = "Player";
         age = 25;
     }
+    [SerializeField] private Transform _transform;
+    [SerializeField] private Transform _transform1;
+    [SerializeField] private Transform _transform2;
+    [SerializeField] private Transform _transform3;
     void Start()
     {
-        DebugLog();
-        DebugFormatLog();
-        DebugBehavior();
-        DebugAssert();
-        DebugLogAssertion();
+        //DebugLog();
+        //DebugFormatLog();
+        //DebugBehavior();
+        ////DebugAssert();
+        ////DebugLogAssertion();
+        //DebugLogException();
+        DebugDrawLine();
     }
     private void Update()
     {
-        Debug.ClearDeveloperConsole();
-        //value+=value*Time.deltaTime;
-        //Debug.Log($"Value: {value}");
-        //if (value > 20 )
-        //{
-        //    //DebugBreak();
-           
-        //}
+        DebugDrawRay();
     }
+    void DebugDrawRay()
+    {
+        Debug.DrawRay(_transform.position,_transform1.position,Color.red);
+    }
+
+    void DebugDrawLine()
+    {
+        Debug.DrawLine(_transform.position, _transform1.position, Color.blue, 20f);
+        Debug.DrawLine(_transform2.position,_transform3.position,Color.cyan,20f,true);
+    }
+    int DebugLogException()
+    {
+        try
+        {
+            int result = 0;
+            int result1 = 0;
+            return result / result1;
+        }
+        catch (Exception e)
+        {
+            Debug.LogException(e);
+            return 0;
+        }
+    }
+    //private void Update()
+    //{
+    
+    //    //Debug.ClearDeveloperConsole();
+    //    //value+=value*Time.deltaTime;
+    //    //Debug.Log($"Value: {value}");
+    //    //if (value > 20 )
+    //    //{
+    //    //    //DebugBreak();
+
+    //    //}
+    //}
+   
     void DebugBreak()
     {
         Debug.Break();
-        Debug.ClearDeveloperConsole();
     }
-    
+
     void DebugLogAssertion()
     {
-        if (age> 18)
+        if (age > 18)
         {
             Debug.LogAssertion("Kullanýcý 18 yaþýndan büyük");
         }
-        Debug.Break();
-        Debug.DebugBreak();
-
     }
     void DebugAssert()
     {
-
         Debug.Assert(age <= 15, "Kullanýcý 18 yaþýndan küçük");
     }
 
